934aafde7171211d209cbd9315f7f9a7623394a9
Gergely Brautigam <skarlso777@gmail.com>
2018-06-05 17:09:52 +0200

    Correct image parsing.

--- index.php
+++ index.php
@@ -6,7 +6,18 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Random RFC</title>
 </head>
+<style>
+    img#background {
+      width: 100%;
+      height: auto;
+      position: fixed;
+      z-index: -1;
+      bottom: 0;
+      right: 0;
+    }
+</style>
 <body>
+    <img id="background" src="background_1.png">
     <script src="https://cdn.jsdelivr.net/npm/vue@2.5.13/dist/vue.js"></script>
     Today's RFC Number is: <h1>
     <?php
